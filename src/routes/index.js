2497477aa4b542abf0585d0b43e54c9ae634a80f
Daniil Ventsov <ventsovdaniil@yandex.ru>
2022-09-10 17:29:59 +0300

    fix: add prettier config file and change project files structure

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,6 @@
 const express = require("express"),
-    router = express.Router(),
-    mainPage = require("./mainPage.routes");
+  router = express.Router(),
+  mainPage = require("./mainPage.routes");
 
 // routes
 router.use("/", mainPage);
@@ -8,25 +8,21 @@ router.use("/", mainPage);
 // error handlers
 // 404 error handler
 router.use((req, res, next) => {
-    let err = new Error("Page Not Found")
-    err.status = 404
-    next(err)
+  let err = new Error("Page Not Found");
+  err.status = 404;
+  next(err);
 });
 // development error handler
-if (router.get('env') === 'development') {
-    router.use((err, req, res, next) => {
-        res.status(err.status || 500);
-        res.send(
-            `Произошла ошибка: ${err.message};  ${err}; `,
-        )
-    });
+if (router.get("env") === "development") {
+  router.use((err, req, res, next) => {
+    res.status(err.status || 500);
+    res.send(`Произошла ошибка: ${err.message};  ${err}; `);
+  });
 }
 
 // production error handler
 router.use((err, req, res, next) => {
-    res.status(err.status || 500);
-    res.send(
-        `Произошла ошибка: ${err.message}`,
-    )
+  res.status(err.status || 500);
+  res.send(`Произошла ошибка: ${err.message}`);
 });
 module.exports = router;
