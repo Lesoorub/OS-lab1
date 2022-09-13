34a37339bae9a306904318647fab24f09a56ce02
Daniil Ventsov <ventsovdaniil@yandex.ru>
2022-09-13 16:07:34 +0300

    fix: add prettier config file

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,19 +1,19 @@
-const express = require("express"),
+const express = require('express'),
   router = express.Router(),
-  mainPage = require("./mainPage.routes");
+  mainPage = require('./mainPage.routes');
 
 // routes
-router.use("/", mainPage);
+router.use('/', mainPage);
 
 // error handlers
 // 404 error handler
 router.use((req, res, next) => {
-  let err = new Error("Page Not Found");
+  let err = new Error('Page Not Found');
   err.status = 404;
   next(err);
 });
 // development error handler
-if (router.get("env") === "development") {
+if (router.get('env') === 'development') {
   router.use((err, req, res, next) => {
     res.status(err.status || 500);
     res.send(`Произошла ошибка: ${err.message};  ${err}; `);
--- src/routes/mainPage.routes.js
+++ src/routes/mainPage.routes.js
@@ -1,8 +1,8 @@
-const express = require("express"),
+const express = require('express'),
   router = express.Router();
 
 router
-  .route("/")
+  .route('/')
   .get((req, res) =>
     res.send(
       'Здесь должна быть фраза "Hello World", но работу выполнили студенты группы 020303-АИСа-о19: <b>Зуев, Павленко, Венцов, Католенец, Скребнев</b>, поэтому надпись изменилась'
