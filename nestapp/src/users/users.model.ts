b5bf3670ae51bc8a0214f3d9576bfdba50bd08c6
sonixkd <113375135+sonixkd@users.noreply.github.com>
2022-09-21 00:59:00 +0300

    Установил nestjs, подключил postgresql, сделал документацию swagger

---
+++ nestapp/src/users/users.model.ts
@@ -0,0 +1,26 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { Column, DataType, Model, Table } from "sequelize-typescript";
+
+//Интерфейс для создания объекта класса User
+interface UserCreationAttrs{
+    email: string;
+    password: string;
+}
+
+@Table({tableName: 'users'})
+export class User extends Model<User, UserCreationAttrs> {
+    
+    @ApiProperty({example: '1', description: 'Уникальный индетификатор'})
+    @Column({type: DataType.INTEGER, unique: true, autoIncrement: true, primaryKey: true})
+    id: number;
+
+    @ApiProperty({example: 'user@mail.ru', description: 'Почтовый адрес'})
+    @Column({type: DataType.STRING, unique: true, allowNull: false})
+    email: string;
+
+    @ApiProperty({example: 'password', description: 'Пароль'})
+    @Column({type: DataType.STRING, allowNull: false})
+    password: string;
+
+
+}
