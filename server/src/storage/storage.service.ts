c3246fbe3caa2243dd2eb6707d19391b60ddc609
Leonid Skrebnev <ristalvtgames@yandex.com>
2022-12-06 19:18:22 +0300

    starting aws

--- server/src/storage/storage.service.ts
+++ server/src/storage/storage.service.ts
@@ -1,27 +1,44 @@
-import { Injectable } from '@nestjs/common';
-import { CreateStorageDto } from './dto/create-storage.dto';
-import { GetStorageDto } from './dto/get-storage.dto';
-import { UpdateStorageDto } from './dto/update-storage.dto';
+import {Injectable} from '@nestjs/common';
+import {CreateStorageDto} from './dto/create-storage.dto';
+import {GetStorageDto} from './dto/get-storage.dto';
+import {UpdateStorageDto} from './dto/update-storage.dto';
 
 @Injectable()
 export class StorageService {
-  create(createStorageDto: CreateStorageDto) {
-    return 'This action adds a new storage';
-  }
+    create(createStorageDto: CreateStorageDto) {
+        return 'This action adds a new storage';
+    }
 
-  getAll(offset:number, count:number) {
-    return `This action returns all storage`;
-  }
+    getAll(offset: number, count: number) {
+        return `This action returns all storage`;
+    }
 
-  get(id: number, getStorageDto: GetStorageDto) {
-    return `This action returns a #${id} storage`;
-  }
+    get(id: number, getStorageDto: GetStorageDto) {
+        var params = {
+            Bucket: getStorageDto.bucket,
+            Key: `${id}`
+        };
+        s3.getObject(params, function (err, data) {
+            if (err) console.log(err, err.stack); // an error occurred
+            else {
+                console.log(data);
+                return data;
+            }
+        });
+    }
 
-  update(id: number, updateStorageDto: UpdateStorageDto) {
-    return `This action updates a #${id} storage`;
-  }
+    update(id: number, updateStorageDto: UpdateStorageDto) {
+        return `This action updates a #${id} storage`;
+    }
 
-  remove(id: number) {
-    return `This action removes a #${id} storage`;
-  }
+    remove(id: number) {
+        return `This action removes a #${id} storage`;
+    }
+
+    getS3() {
+        return new S3({
+            accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+            secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+        });
+    }
 }
