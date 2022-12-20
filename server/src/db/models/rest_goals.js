f9a1667006e06a799a4a30e33b47a64850c1bc16
Leonid Skrebnev <ristalvtgames@yandex.com>
2022-12-20 13:42:15 +0300

    revert comit

--- server/src/db/models/rest_goals.js
+++ server/src/db/models/rest_goals.js
@@ -1,26 +1,25 @@
 'use strict';
-const { stat, Stats } = require('fs');
 const {
     Model
 } = require('sequelize');
-
 module.exports = (sequelize, DataTypes) => {
-    class Rest_goals extends Model { 
+    class Rest_goals extends Model {
+        /**
+         * Helper method for defining associations.
+         * This method is not a part of Sequelize lifecycle.
+         * The `models/index` file will call this method automatically.
+         */
         static associate(models) {
-            //this.belongsTo(models.Stats, {as: 'stat'})
+            // define association here
         }
     }
 
     Rest_goals.init({
         text: DataTypes.TEXT,
-        is_done: DataTypes.BOOLEAN,
-        is_done2: DataTypes.BOOLEAN
+        is_done: DataTypes.BOOLEAN
     }, {
         sequelize,
         modelName: 'Rest_goals',
     });
-
-    Rest_goals.stat = Rest_goals.belongsTo(Stats)
-
     return Rest_goals;
 };
--- server/src/db/models/stats.js
+++ server/src/db/models/stats.js
@@ -4,8 +4,13 @@ const {
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
     class Stats extends Model {
+        /**
+         * Helper method for defining associations.
+         * This method is not a part of Sequelize lifecycle.
+         * The `models/index` file will call this method automatically.
+         */
         static associate(models) {
-            this.belongsTo(models.User, {as: 'user'})
+            // define association here
         }
     }
 
